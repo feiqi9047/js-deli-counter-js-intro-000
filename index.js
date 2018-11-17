fc3c2791cb8b3ccf017fc0d067d9bbcc46da7fdb
Fei Qi <feiqi9047@gmail.com>
2018-11-17 21:21:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ function nowServing(katzDeliLine){
   if (katzDeliLine.length ===0){
     katzDeliLine.push("There is nobody waiting to be served!")
   }else{
-    katzDeliLine[0]
+    return 
   }
-  return katzDeliLine
 }
