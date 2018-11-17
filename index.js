0403c2a8770b78aab13bb6fe280fc76d7b1c7b08
Fei Qi <feiqi9047@gmail.com>
2018-11-17 21:14:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name){
 var array = []
-let line = katzDeliLine.length
+let line = katzDeliLine.length + 1
 array.push("Welcome, " + name + ". You are number " + line + " in line.")
 return array
 }
